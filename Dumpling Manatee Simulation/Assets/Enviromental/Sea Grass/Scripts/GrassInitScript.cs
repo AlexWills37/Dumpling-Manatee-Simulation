916fc7c8c8d83b4565c3228cb2a47bdacd4978ed
RileyWood938 <71392554+RileyWood938@users.noreply.github.com>
2023-06-15 12:51:22 -0600

    Documentation updated
    changed sea grass documentation to fit markdown format (I hope). Commented scripts and made variable names better

--- Dumpling Manatee Simulation/Assets/Enviromental/Sea Grass/Scripts/GrassInitScript.cs
+++ Dumpling Manatee Simulation/Assets/Enviromental/Sea Grass/Scripts/GrassInitScript.cs
@@ -2,14 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// A simple script which holds the initialization variables and logic for every sea grass.
+/// </summary>
 public class GrassInitScript : MonoBehaviour
 {
-    [SerializeField]
-    private string startDirection;
+    [SerializeField] [Tooltip("x, -x, y, or -y")]
+    private string startDirection; //the direction that the grass hsould start in
 
+    /// <summary>
+    /// gets the shape key that the grass is meant to start bending along
+    /// </summary>
+    /// <returns>returns an integer representing the index of the correct shape key</returns>
     public int GetShapeKeyRotation()
     {
-        switch (startDirection)
+        switch (startDirection) //simply convert the name of the axis to the correct shape key index
         {
             case "x":
                 return 0;
@@ -20,7 +27,7 @@ public class GrassInitScript : MonoBehaviour
             case "-y":
                 return 3;
             default:
-                throw new System.Exception("Bad Grass Rotation");
+                throw new System.Exception("Bad Grass Rotation"); //if an incorrect value has been input, throw an error
         }
        
     }
